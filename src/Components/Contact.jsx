9669c15804e173cc795985236b9dedd62949ee3e
Sarath Teja revu <sarathteja14@gmail.com>
2024-04-21 01:15:27 +0530

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import emailjs from "@emailjs/browser";
+import emailjs from "emailjs-com";
 import Swal from "sweetalert2";
 
 const Contact = () => {
